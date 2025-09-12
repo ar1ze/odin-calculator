fc5ac8aa63b4ed35a3ec9fc9999bb34d56cc1172
Eric Le <eriiicle@outlook.com>
2025-09-12 18:44:51 +0200

    refactor(calculator): improve add to sequence logic

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const DECIMAL_POINT = '.';
 const OPERATORS = ['+', '-', 'x', '/', '%'];
-const PLUS_MINUS_SYMBOLS = ['(', '-', ')'];
+const NEGATION_WRAPPER_CHARS = ['(', '-', ')'];
 
 // Stores the current calculation sequence of operands and operators.
 let calculationSequence = ['0'];
@@ -59,19 +59,19 @@ function handleEqualsClick() {
   console.log(`The button equals '=' was clicked!`);
 }
 
-function isPartToggled(partAsArray) {
-  let plusMinusSymbolCount = partAsArray.reduce((sum, currentCharacter) => {
-    let isPlusMinusSymbol = PLUS_MINUS_SYMBOLS.includes(currentCharacter);
-    if (isPlusMinusSymbol) return ++sum;
+function isPartNegated(partAsArray) {
+  let negationSymbolCount = partAsArray.reduce((sum, currentCharacter) => {
+    let isNegationSymbol = NEGATION_WRAPPER_CHARS.includes(currentCharacter);
+    if (isNegationSymbol) return ++sum;
     else return sum;
   }, 0);
-  return plusMinusSymbolCount === 3;
+  return negationSymbolCount === 3;
 }
 
 function convertToPositivePart(partAsArray) {
   return partAsArray.reduce((accumulatedString, currentCharacter) => {
-    let isPlusMinusSymbol = PLUS_MINUS_SYMBOLS.includes(currentCharacter);
-    if (!isPlusMinusSymbol) return accumulatedString + currentCharacter;
+    let isNegationSymbol = NEGATION_WRAPPER_CHARS.includes(currentCharacter);
+    if (!isNegationSymbol) return accumulatedString + currentCharacter;
     else return accumulatedString;
   }, '');
 }
@@ -82,111 +82,130 @@ function convertToNegativePart(part) {
 
 function togglePlusMinus() {
   let isCalculationInitiallyZero = calculationSequence.at(0) === '0';
-  let lastPart = calculationSequence.slice(-1).at(0);
-  let isLastPartAnOperator = OPERATORS.includes(lastPart);
+  let lastElement = calculationSequence.slice(-1).at(0);
+  let isLastElementAnOperator = OPERATORS.includes(lastElement);
 
   // Do nothing if display is "0" or the last entry is an operator.
-  if (isCalculationInitiallyZero || isLastPartAnOperator) return;
+  if (isCalculationInitiallyZero || isLastElementAnOperator) return;
 
-  let lastPartAsArray = lastPart.split('');
-  let isCurrentlyToggled = isPartToggled(lastPartAsArray);
-  let toggledPart;
+  let lastElementAsArray = lastElement.split('');
+  let isCurrentlyNegated = isPartNegated(lastElementAsArray);
+  let toggledElement;
 
-  if (isCurrentlyToggled) {
-    toggledPart = convertToPositivePart(lastPartAsArray);
+  if (isCurrentlyNegated) {
+    toggledElement = convertToPositivePart(lastElementAsArray);
   } else {
-    toggledPart = convertToNegativePart(lastPart);
+    toggledElement = convertToNegativePart(lastElement);
   }
 
-  calculationSequence.splice(-1, 1, toggledPart);
+  calculationSequence.splice(-1, 1, toggledElement);
+}
+
+function insertNewElementToCalculation(inputValue) {
+  let lastElement = calculationSequence.slice(-1).at(0);
+  let isLastElementAnOperator = OPERATORS.includes(lastElement);
+  let isInputADecimal = inputValue === DECIMAL_POINT;
+
+  // Initalize input to the operator input
+  let newValue = inputValue;
+
+  // Add "0." after an operator if a decimal is pressed.
+  if (isLastElementAnOperator && isInputADecimal) {
+    newValue = '0' + inputValue;
+  }
+
+  calculationSequence.push(newValue);
+}
+
+function appendToLastElementInCalculation(inputValue) {
+  let isInputADecimal = inputValue === DECIMAL_POINT;
+
+  let lastElement = calculationSequence.slice(-1).at(0);
+  let lastElementAsArray = lastElement.split('');
+
+  let lastElementContainsDecimal = lastElement.includes(DECIMAL_POINT);
+
+  // Prevent multiple decimals in one number.
+  if (lastElementContainsDecimal && isInputADecimal) return;
+
+  let isNegated = isPartNegated(lastElementAsArray);
+  if (isNegated) {
+    // Add multiply operator before the input if the last part is toggled negative.
+    calculationSequence.push('x');
+
+    // Append zero before decimal if needed.
+    if (isInputADecimal) {
+      inputValue = '0' + inputValue;
+    }
+
+    // Insert into the sequence
+    calculationSequence.push(inputValue);
+  } else {
+    // Add to the multi digit
+    let updatedElement = lastElement + inputValue;
+    let isCalculationInitiallyZero = calculationSequence.at(0) === '0';
+
+    // If iniital value is zero just replace it
+    if (isCalculationInitiallyZero) {
+      updatedElement = inputValue;
+    }
+
+    calculationSequence.splice(-1, 1, updatedElement);
+  }
 }
 
 // Adds user input to the calculation sequence, handling complex logic.
 function addToCalculation(inputValue) {
-  let isCalculationInitiallyZero = calculationSequence.at(0) === '0';
+  let isCalculationEmpty = calculationSequence.length === 0;
 
   let isInputAnOperator = OPERATORS.includes(inputValue);
   let isInputADecimal = inputValue === DECIMAL_POINT;
 
-  let lastPart = calculationSequence.slice(-1).at(0);
-  let lastPartAsArray = lastPart.split('');
-  let lastCharOfLastPart = lastPart.slice(-1);
+  let lastElement = calculationSequence.slice(-1).at(0);
+  let lastCharOfLastElement = lastElement.slice(-1);
 
-  let isLastPartAnOperator = OPERATORS.includes(lastPart);
-  let isLastCharOfLastPartADecimal = lastCharOfLastPart === DECIMAL_POINT;
-  let lastPartContainsDecimal = lastPart.includes(DECIMAL_POINT);
+  let isLastElementAnOperator = OPERATORS.includes(lastElement);
+  let isLastCharOfLastElementADecimal = lastCharOfLastElement === DECIMAL_POINT;
+  let isInputOrLastElementAnOperator =
+    isInputAnOperator || isLastElementAnOperator;
 
-  let isInputOrLastPartAnOperator = isInputAnOperator || isLastPartAnOperator;
-  let areInputAndLastPartOperators = isInputAnOperator && isLastPartAnOperator;
-  let areInputAndLastCharDecimals =
-    isInputADecimal && isLastCharOfLastPartADecimal;
+  let areInputAndLastElementOperators =
+    isInputAnOperator && isLastElementAnOperator;
+  let areInputAndLastCharBothDecimals =
+    isInputADecimal && isLastCharOfLastElementADecimal;
 
   // Prevent invalid inputs like consecutive operators or decimals.
-  if (areInputAndLastPartOperators) return;
-  if (areInputAndLastCharDecimals) return;
+  if (areInputAndLastElementOperators || areInputAndLastCharBothDecimals)
+    return;
 
-  // Handle replacing the initial '0'.
-  if (isCalculationInitiallyZero && !areInputAndLastPartOperators) {
-    if (isInputADecimal && isCalculationInitiallyZero) {
-      // Convert "0" to "0."
-      calculationSequence.splice(-1, 1, lastPart + inputValue);
-    } else {
-      // Remove leading zero for new numbers.
-      calculationSequence.shift();
-    }
-  }
-
-  let isCalculationEmpty = calculationSequence.length === 0;
   // Decide whether to start a new calculation part or append to the current one.
-  if (isInputOrLastPartAnOperator || isCalculationEmpty) {
-    if (isLastPartAnOperator && isInputADecimal) {
-      // Add "0." after an operator if a decimal is pressed.
-      calculationSequence.push('0' + inputValue);
-    } else {
-      calculationSequence.push(inputValue);
-    }
+  if (isInputOrLastElementAnOperator || isCalculationEmpty) {
+    insertNewElementToCalculation(inputValue);
   } else {
-    // Prevent multiple decimals in one number.
-    if (lastPartContainsDecimal && isInputADecimal) return;
-
-    let isToggled = isPartToggled(lastPartAsArray);
-    if (isToggled) {
-      // Add multiply operator before the input if the last part is toggled negative.
-      calculationSequence.push('x');
-
-      // Append zero before decimal if needed.
-      if (isInputADecimal) {
-        inputValue = '0' + inputValue;
-      }
-
-      calculationSequence.push(inputValue);
-    } else {
-      calculationSequence.splice(-1, 1, lastPart + inputValue);
-    }
+    appendToLastElementInCalculation(inputValue);
   }
 }
 
 // Removes the last character or element from the calculation array.
 function deleteLastInput() {
-  let lastPart = calculationSequence.slice(-1).at(0);
-  let lastPartAsArray = lastPart.split('');
-
-  let isLastPartAnOperator = OPERATORS.includes(lastPart);
-  let isCalculationASinglePart = calculationSequence.length === 1;
-  let isLastPartASingleChar = lastPartAsArray.length === 1;
+  let lastElement = calculationSequence.slice(-1).at(0);
+  let lastElementAsArray = lastElement.split('');
+  let isLastElementAnOperator = OPERATORS.includes(lastElement);
+  let isSingleElementCalculation = calculationSequence.length === 1;
+  let isLastElementSingleChar = lastElementAsArray.length === 1;
 
   // Reset to '0' if deleting the very last character on screen.
-  if (isCalculationASinglePart && isLastPartASingleChar) {
+  if (isSingleElementCalculation && isLastElementSingleChar) {
     clearCalculation();
     return;
   }
 
-  if (isLastPartAnOperator || isLastPartASingleChar) {
+  if (isLastElementAnOperator || isLastElementSingleChar) {
     calculationSequence.pop();
   } else {
     // Remove the last character from a multi-digit number.
-    lastPartAsArray.pop();
-    calculationSequence.splice(-1, 1, lastPartAsArray.join(''));
+    lastElementAsArray.pop();
+    calculationSequence.splice(-1, 1, lastElementAsArray.join(''));
   }
 }
 
@@ -195,7 +214,6 @@ function clearCalculation() {
 }
 
 function updateDisplay() {
-  console.log(calculationSequence);
   calculationDisplay.textContent =
     formatCalculationForDisplay(calculationSequence);
 }
